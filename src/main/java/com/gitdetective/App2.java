ca1c5f2e74ebc1fdb8fa90689e2b665aa6b962f7
Brandon Fergerson <fergerson92@gmail.com>
2020-02-15 10:08:16 -0500

    more mini

--- src/main/java/com/gitdetective/App2.java
+++ src/main/java/com/gitdetective/App2.java
@@ -1,7 +1,7 @@
 package com.gitdetective;
 
 public class App2 {
-    public static void main(String[] args) {
+    public static void main() {
         System.out.println("Hello World!");
     }
 }
